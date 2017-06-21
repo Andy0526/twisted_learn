fdc6b56a9f3097a85e74a0a592e176e9fdf1e3c4
liujintong <liujintong@peiwo.cn>
2017-06-21 19:01:45 +0800

    fix

---
+++ basic_twisted/hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:lewsan
+
+from twisted.internet import reactor
+import traceback
+
+def hello():
+    traceback.print_stack()
+    print 'Hello from the reactor loop!'
+    print 'Lately I feel like I\'m stuck in a rut.'
+
+print type(reactor),reactor
+
+reactor.callWhenRunning(hello)
+print 'Starting the reactor'
+reactor.run()
