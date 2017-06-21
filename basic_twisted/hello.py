0bbbef928eacc9956fc580e79bebe488bde43f10
lewsan <532235905@qq.com>
2017-06-21 22:18:20 +0800

    fix

--- basic_twisted/hello.py
+++ basic_twisted/hello.py
@@ -5,12 +5,14 @@
 from twisted.internet import reactor
 import traceback
 
+
 def hello():
     traceback.print_stack()
     print 'Hello from the reactor loop!'
     print 'Lately I feel like I\'m stuck in a rut.'
 
-print type(reactor),reactor
+
+print type(reactor), reactor
 
 reactor.callWhenRunning(hello)
 print 'Starting the reactor'
