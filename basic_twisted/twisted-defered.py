9b76858d11e50af9b70b537f436d6faeacf195d9
liujintong <liujintong@peiwo.cn>
2017-07-10 12:43:48 +0800

    fix

--- basic_twisted/twisted-defered.py
+++ basic_twisted/twisted-defered.py
@@ -11,18 +11,26 @@ from twisted.internet import defer
 import traceback
 
 
+def got_results1():
+    print 'got_resilts1'
+
+
 def got_results(res):
     print 'we got:', res
-    traceback.print_stack()
+    d = defer.Deferred()
+    d.addCallback(got_results1)
+    return d
+    # traceback.print_stack()
 
 
 # d = defer.DeferredList([])
 #
 # d.addCallback(got_results)
 
-d1 = defer.Deferred()
-d2 = defer.Deferred()
-d = defer.DeferredList([d1, d2])
-d.addCallback(got_results)
-d1.callback('d1 result')
-d2.callback('d2 result')
+# d1 = defer.Deferred()
+# d2 = defer.Deferred()
+# d = defer.DeferredList([d1, d2])
+# d.addCallback(got_results)
+# d1.callback('d1 result')
+# d2.callback('d2 result')
+defer.DeferredList([got_results('d1'), got_results('d2')])
